4ea089a1d6a3663465c8bacbb8d66232f353ae53
Artem <muha13artem@gmail.com>
2025-11-26 17:13:00 +0200

    abdate js

--- my-project/src/scripts/main.js
+++ my-project/src/scripts/main.js
@@ -4,8 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const burger = document.getElementById('burger');
 
   burger.addEventListener('click', () => {
-    burger.classList.toggle('icon--menu');
-    burger.classList.toggle('icon--menu-click');
-    // href="#menu" працює автоматично, прокрутка до меню спрацьовує
+    if (burger.classList.contains('icon--menu')) {
+      burger.classList.remove('icon--menu');
+      burger.classList.add('icon--menu-click');
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const close = document.getElementById('close');
+  const burger = document.getElementById('burger');
+
+  close.addEventListener('click', () => {
+    if (burger.classList.contains('icon--menu-click')) {
+      burger.classList.remove('icon--menu-click');
+      burger.classList.add('icon--menu');
+    }
   });
 });
